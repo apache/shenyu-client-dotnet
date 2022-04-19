da26aa306ebed0850fb7358eaa1f5e25ec9e675c
hgaol <dhangao@hotmail.com>
2022-04-19 10:54:51 +0800

    add apache 2.0 license and remove .editorconfig file

--- examples/AspNetCoreExample/Controllers/TemplateController.cs
+++ examples/AspNetCoreExample/Controllers/TemplateController.cs
@@ -1,9 +1,12 @@
 ﻿/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -25,7 +28,7 @@ namespace AspNetCoreExample.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class Test2Controller
+    public class TemplateController
     {
         private static readonly string[] Summaries = new[]
         {
@@ -34,7 +37,7 @@ namespace AspNetCoreExample.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public Test2Controller(ILogger<WeatherForecastController> logger, IServer server)
+        public TemplateController(ILogger<WeatherForecastController> logger, IServer server)
         {
             this._logger = logger;
             var addresses = server.Features.Get<IServerAddressesFeature>().Addresses;
